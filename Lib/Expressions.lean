ea9c069c03ab9d1fda1ef4ec5a6fda1c67162c03
lucasarruda12 <lucas123manoel4@gmail.com>
2025-04-16 09:26:13 -0300

    Expressions done

--- Lib/Expressions.lean
+++ Lib/Expressions.lean
@@ -10,20 +10,20 @@ def exprNat : Nat → Expr
 def one := exprNat (Nat.succ Nat.zero)
 def two := exprNat (Nat.succ $ Nat.succ $ Nat.zero)
 
--- 2 + 1 :)
+-- 1. 2 + 1 :)
 #eval Expr.app (Expr.app (Expr.const ``Nat.add []) one) two
 
--- 2 + 1 (2) :)
+-- 2. 2 + 1 (2) :)
 #check Lean.mkAppN
 #eval Lean.mkAppN (Expr.const ``Nat.add []) #[one, two]
 
--- λ x => 1 + x
+-- 3. λ x => 1 + x
 #check Expr.lam
 #check Expr.lam `x (Expr.const ``Nat []) (
     Lean.mkAppN (Expr.const ``Nat.add []) #[(Expr.bvar 0), one]
   ) Lean.BinderInfo.default
 
--- λ a => λ b => λ c => (a + b) * c
+-- 4. λ a => λ b => λ c => (a + b) * c
 def exNat : Expr := Expr.const ``Nat []
 def exAdd : Expr := Expr.const ``Nat.add []
 def myLam : Expr := Expr.lam `a (exNat) (
@@ -36,16 +36,36 @@ def myLam : Expr := Expr.lam `a (exNat) (
 
 #eval Lean.mkAppN myLam #[zero, one, two]
 
--- λ x y => x + y
+-- 5. λ x y => x + y
 #check Expr.lam `x exNat (
   Expr.lam `y exNat (
     Lean.mkAppN exAdd #[Expr.bvar 1, Expr.bvar 0]
   ) Lean.BinderInfo.default
 ) Lean.BinderInfo.default
 
--- λ x => String.append "Hello " x
+-- 6. λ x => String.append "Hello " x
 #check Expr.lam `x (Expr.const ``String []) (
   Lean.mkAppN (Expr.const ``String.append []) #[
     (Expr.lit $ Literal.strVal "Hello "), Expr.bvar 0
   ]
 ) Lean.BinderInfo.default
+
+-- 7. ∀x : Prop, x ∧ x
+#check Expr.forallE `x (Expr.sort Level.zero) (
+  Lean.mkAppN (Expr.const ``And.intro []) #[Expr.bvar 0, Expr.bvar 0]
+) Lean.BinderInfo.default
+
+-- 8. Nat → String
+#check Expr.forallE `x
+  (exNat) (Expr.const ``String []) Lean.BinderInfo.default
+
+-- 9. λ (p : Prop) => (λ hP : p => hP)
+#check Expr.lam `p (Expr.sort Level.zero) (
+  Expr.lam `hp (Expr.bvar 1) (Expr.bvar 0) Lean.BinderInfo.default
+) Lean.BinderInfo.default
+
+-- 10. Type 6
+def natToLevel : Nat → Level
+  | Nat.zero => Level.zero
+  | Nat.succ n => Level.succ (natToLevel n)
+#check Expr.sort (natToLevel 6)
